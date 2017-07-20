1baea845d216157fee504f8d6b29ba996a63af75
Sagar Routh <sagarrouth0512@gmail.com>
2017-07-20 18:10:30 +0530

    Start server integration

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
 const path = require('path');
 
 const config = {
-  entry: './components/App.js',
+  entry: './public/components/App.js',
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'app.bundle.js'
   },
   module: {
     rules: [
-      {include: path.resolve(__dirname, 'components'), test:/\.(js)$/, use: 'babel-loader'},
+      {include: path.resolve(__dirname, 'public/components'), test:/\.(js)$/, use: 'babel-loader'},
       {test: /\.(css)$/, use: [ 'style-loader', { loader: 'css-loader', options: { url: false } }]}
     ]
   }
